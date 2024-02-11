30a5dc0e2c55aaddb08e83bc2216e7fa7e702ed6
kofmania <kofmania@gmail.com>
2024-02-12 00:34:21 +0900

    update pages

---
+++ src/app/(maketing)/layout.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "@/components/ui/footer";
+import { Navbar } from "@/components/ui/navbar";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
